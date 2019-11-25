cc637b8f551d7b2f7deaf42034a52d10a144c2fc
Bryce Eller <imbrycecold@gmail.com>
2019-11-25 16:38:45 -0500

    Update Python file

--- A4/qlearn.py
+++ A4/qlearn.py
@@ -1,9 +1,8 @@
-import random
+### import random
 import sys
-
+import random
 
 DEFAULT_STATE = '       | ###  -| # #  +| # ####|       '
-
 
 class Action:
 
@@ -104,37 +103,118 @@ class Env:
 class QTable:
 
     def __init__(self, env, actions):
-        # initialize your q table
+        xSize = env.x_size
+        ySize = env.y_size
+        self.env = env
+        self.actions = actions
+        self.table = []
+        for x in range(xSize):
+            row = []
+            for y in range(ySize):
+                row.append([0,0,0,0])
+            self.table.append(row)
+        
 
     def get_q(self, state, action):
         # return the value of the q table for the given state, action
+        index = ACTIONS.index(action)
+        x = state.x
+        y = state.y
+        
+        q = self.table[x][y][index]
+        return q
 
     def get_q_row(self, state):
         # return the row of q table corresponding to the given state
+        x = state.x
+        y = state.y
+        return self.table[x][y]
 
     def set_q(self, state, action, val):
         # set the value of the q table for the given state, action
+        index = ACTIONS.index(action)
+        x = state.x
+        y = state.y
+        
+        self.table[x][y][index] = val
+        return True
 
-    def learn_episode(self, alpha=.10, gamma=.90):
+    def learn_episode(self, alpha=.1, gamma=.9):
         # with the given alpha and gamma values,
         # from a random initial state,
         # consider a random legal action, execute that action,
         # compute the reward, and update the q table for (state, action).
         # repeat until an end state is reached (thus completing the episode)
         # also print the state at each state
-    
-    def learn(self, episodes, alpha=.10, gamma=.90):
-        # run <episodes> number of episodes for learning with the given alpha and gamma
+        
+        env = self.env
+        state = env.random_state()
+        end = False
+        
+        while not end:
+            print(state)
+            legalActions = state.legal_actions(ACTIONS)
+            action = legalActions[random.randint(0,len(legalActions)-1)]
 
+            oldQ = self.get_q(state,action)
+            oldState = state.clone()
+
+            state.execute(action)
+            reward = state.reward()
+            if reward is not None and reward is not 0:
+                end=True
+            row = self.get_q_row(state)
+            maxA = max(row)
+
+            q = ((1-alpha)*oldQ) + (alpha*(reward+(gamma*maxA)))
+            self.set_q(oldState, action, q)
+    
+    def learn(self, episodes, alpha=.1, gamma=.9):
+        for i in range(episodes):
+            self.learn_episode(alpha, gamma)
+    
+    def invertArray(self, arr):
+        result = []
+        for i in range(len(arr[0])):
+            newRow = []
+            for row in arr:
+                newRow.append(row[i])
+            result.append(newRow)
+        return result
+    
     def __str__(self):
         # return a string for the q table as described in the assignment
+        result = ""
+        actionIndex=0
+        
+        invertedTable = self.invertArray(self.table)
+        
+        for action in ACTIONS:
+            result+= action.name + "\n"
+            for row in invertedTable:
+                for box in row:
+                    if box[actionIndex] != 0:
+                        result += str(round(box[actionIndex],2))
+                    else:
+                        result += "----"
+                    result +="\t"
+                result += "\n"
+            actionIndex+=1
+        return result
+    
 
-
+        
 if __name__ == "__main__":
     if len(sys.argv) > 1:
         cmd = sys.argv[1]
-        env = Env(sys.argv[2] if len(sys.argv) > 2 else DEFAULT_STATE)
+        env = Env(DEFAULT_STATE)
+        
         if cmd == 'learn':
             qt = QTable(env, ACTIONS)
             qt.learn(100)
             print(qt)
+
+#     env = Env(DEFAULT_STATE)
+#     qt = QTable(env, ACTIONS)
+#     qt.learn(100)
+#     print(qt)
